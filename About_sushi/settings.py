dfe316b69cae72b701433be873e6818dd16da1b3
dimat82 <dimat82@list.ru>
2018-09-30 10:32:34 +0300

    Site sushi, second commit

--- About_sushi/settings.py
+++ About_sushi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=+0$(%hjsag9%=_j3%u9gf4vst4w9p(@$hxjy5(f!&vh+bqf0='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dimat82.pythonanywhere.com']
 
 
 # Application definition
